dcd2e2dc8076dce16d9a9a818be1873d25c460e3
Aasmi Dhungana <aasmidhungana@Aasmis-MacBook-Air.local>
2025-08-16 15:04:37 +0545

    Trigger frontend rebuild for updated menu API endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import TableBanner from './components/TableBanner';
